ab3f3aba4ad40be1922ef3bdee28a751b27a7e49
oumaimaake <oumaimaakenfar1@gmail.com>
2024-07-02 00:31:29 +0000

    0x02-react_props

---
+++ 0x02-react_props/task_4/dashboard/src/App.test.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import App from './App';
+
+describe('<App />', () => {
+  it('renders without crashing', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.exists()).toBe(true);
+  });
+
+  it('renders a div with the class App-header', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find('div.App-header').exists()).toBe(true);
+  });
+
+  it('renders a div with the class App-body', () => {
+
