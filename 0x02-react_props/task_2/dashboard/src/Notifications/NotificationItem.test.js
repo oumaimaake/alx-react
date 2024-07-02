c099b0f6ac791634b9269d23fc75d5b1669e35d4
oumaimaake <oumaimaakenfar1@gmail.com>
2024-07-02 01:41:58 +0000

    task_2

---
+++ 0x02-react_props/task_2/dashboard/src/Notifications/NotificationItem.test.js
@@ -0,0 +1,22 @@
+import React from "react";
+import NotificationItem from "./NotificationItem";
+import { shallow } from "enzyme";
+
+describe("rendering components", () => {
+  it("renders NotificationItem component without crashing", () => {
+    const wrapper = shallow(<NotificationItem />);
+
+    expect(wrapper.exists()).toBe(true);
+  });
+
+  it('renders correct html from type="default" value="test" props', () => {
+    const wrapper = shallow(<NotificationItem />);
+
+    wrapper.setProps({ type: "default", value: "test" });
+    expect(wrapper.html()).toEqual('<li data-notification-type="default">test</li>');
+  });
+
+  it('renders correct html from  html="<u>test</u>" props', () => {
+    const wrapper = shallow(<NotificationItem />);
+
+
