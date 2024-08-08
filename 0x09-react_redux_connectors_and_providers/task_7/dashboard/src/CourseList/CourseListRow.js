b1cc9a729cf9ff778e7806b1f63346507846d6c9
oumaimaake <oumaimaakenfar1@gmail.com>
2024-08-08 01:09:44 +0100

    0x09-react_redux_connectors_and_providers

---
+++ 0x09-react_redux_connectors_and_providers/task_7/dashboard/src/CourseList/CourseListRow.js
@@ -0,0 +1,23 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function CourseListRow({ isHeader, textFirstCell, textSecondCell }) {
+  return (
+    <tr>
+      {isHeader ? (
+        textSecondCell === null ? (
+          <th colSpan={2}>{textFirstCell}</th>
+        ) : (
+          <>
+            <th>{textFirstCell}</th>
+            <th>{textSecondCell}</th>
+          </>
+        )
+      ) : (
+        <>
+          <td>{textFirstCell}</td>
+          <td>{textSecondCell}</td>
+        </>
+      )}
+    </tr>
+
